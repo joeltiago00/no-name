a6e7d739f68cd3f6a3b176af47aa55d37d3f60d0
alvez2g <joeltiago00745@gmail.com>
2023-04-29 01:09:36 -0300

    test

--- src/Igrejei/Post/PostLike.php
+++ src/Igrejei/Post/PostLike.php
@@ -11,8 +11,7 @@ class PostLike
     public function __construct(
         private PostLikeRepository $postLikeRepository,
         private PostRepository $postRepository
-    )
-    {
+    ){
     }
 
     /**
